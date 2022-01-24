3c2f5c124fcd56b91f3e59340f12dad5d97f55c9
allaoua-rico <allaoua.boudriou@gmail.com>
2022-01-24 18:14:58 +0100

    next-app

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
+const name = 'BOUDRIOU Allaoua'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
